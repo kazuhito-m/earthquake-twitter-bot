11b900618c7bb7be29db7839caf2c3ac6364163e
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-09-05 23:20:34 +0900

    実装。地震情報が「同一か」を判定するメソッドを追加。テストも実装。

--- earthquake/earthquake_report.go
+++ earthquake/earthquake_report.go
@@ -5,13 +5,17 @@ import (
 )
 
 type EarthquakeReport struct {
-	Result EarthquakeResult
-	Report_Id string
+	Result        EarthquakeResult
+	Report_Id     string
+	Region_Name   string
+	Calcintensity string
+	Report_Time   string
+	Request_Time  string
 }
 
 type EarthquakeResult struct {
-	Status   string
-	Message  string
+	Status  string
+	Message string
 }
 
 func ParseJsonOf(jsonText string) EarthquakeReport {
@@ -21,6 +25,10 @@ func ParseJsonOf(jsonText string) EarthquakeReport {
 	return info
 }
 
-func (report EarthquakeReport) Exists() bool {
-	return report.Result.Message != "データがありません"
+func (this EarthquakeReport) Exists() bool {
+	return this.Result.Message != "データがありません"
+}
+
+func (this EarthquakeReport) Same(other EarthquakeReport) bool {
+	return this.Report_Id == other.Report_Id
 }
