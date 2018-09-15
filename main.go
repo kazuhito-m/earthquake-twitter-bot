c3e7a278cdf9705edb4f57a72a65ad222b4cbc77
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-09-15 23:44:37 +0900

    実装。一通り「道具が揃った」ので、main関数で「組み立てて実行」するようにする。

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"github.com/kazuhito-m/earthquake-twitter-bot/bot"
 	"github.com/kazuhito-m/earthquake-twitter-bot/config"
+	"github.com/kazuhito-m/earthquake-twitter-bot/earthquake"
 	"github.com/kazuhito-m/earthquake-twitter-bot/twitter"
+	"github.com/kazuhito-m/earthquake-twitter-bot/web"
 	"os"
 )
 
 func main() {
 	settingFilePath := os.Args[1]
 	settings := config.LoadSettings(settingFilePath)
-	twitterTool := twitter.CreateTwitter(settings.Twitter)
-	twitterTool.Tweet(settingFilePath)
+	twitter := twitter.CreateTwitter(settings.Twitter)
+	site := earthquake.CreateSite(web.CreateClient())
+	bot := bot.CreateBot(settings, twitter, site)
+
+	bot.Run()
 }
+
