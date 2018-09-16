c64efb5c2a9a677500e1bdeaa0f55eb6c27be120
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-09-17 02:43:27 +0900

    refactor: 一度インライン展開して「依存性がないきれいなカタチならどうなるか」を見る。

--- coordinate2d/polygon.go
+++ coordinate2d/polygon.go
@@ -7,26 +7,22 @@ type Polygon struct {
 func (p Polygon) InsideOf(target Point) bool {
 	points := p.Points
 	inside := false
-	var cp Point
 	for i, nowPoint := range points {
 		previousPoint := previousPoint(i, points)
 		line := Line{nowPoint, previousPoint}
-		if !cross(target.Y, line, &cp) {
-			continue
-		}
-		if cp.X > target.X {
+
+		p1 := line.First()
+		p2 := line.Last()
+		y := target.Y
+		x := (p1.X*(p2.Y-y) + p2.X*(y-p1.Y)) / (p2.Y - p1.Y);
+
+		cross := p1.Y <= y && y < p2.Y && x > target.X
+
+		if cross {
 			inside = !inside
 		}
 	}
 	return inside;
-}
-
-func cross(y float64, line Line, cp *Point) bool {
-	p1 := line.First()
-	p2 := line.Last()
-	cp.X = (p1.X*(p2.Y-y) + p2.X*(y-p1.Y)) / (p2.Y - p1.Y);
-	cp.Y = y;
-	return p1.Y <= y && y < p2.Y
 }
 
 func previousPoint(index int, points []Point) Point {
