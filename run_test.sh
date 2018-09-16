202b409315e05442c411f3457af8db80c133e12e
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-09-17 07:30:00 +0900

    テストスクリプト変更。カバレッジを coverage というディレクトリに出力するように。 ただ、今のプログラムの構成でテストの置き場が間違っているので、正しいカバレッジが取れない。また変更する。g

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+COVERAGE_DIR=./coverage
+
 GOCACHE=off
-go test -v ./test/...
+
+rm -rf ${COVERAGE_DIR}
+mkdir -p ${COVERAGE_DIR}
+
+go test -coverprofile=${COVERAGE_DIR}/cover.out -covermode=count -cover  ./test/...
+go tool cover -html=${COVERAGE_DIR}/cover.out -o ${COVERAGE_DIR}/index.html
