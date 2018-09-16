6068ec657b4eb4b55ec40a6ebe6dc65d63da361a
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-09-16 14:12:49 +0900

    デバッグ実装。メッセージに他のJson値も出来る限り出力。

--- bot/message_maker.go
+++ bot/message_maker.go
@@ -3,6 +3,7 @@ package bot
 import (
 	"github.com/kazuhito-m/earthquake-twitter-bot/config"
 	"github.com/kazuhito-m/earthquake-twitter-bot/earthquake"
+	"strconv"
 )
 
 type MessageMaker struct {
@@ -11,10 +12,17 @@ type MessageMaker struct {
 
 func (m MessageMaker) Message(report earthquake.EarthquakeReport) string {
 	// TODO 実装
-	message := "ReportId:" + report.Report_Id  + ",ReportTime" + report.Report_Time + ",RequestTime:"  + report.Request_Time
+	message := "ReportId:" + report.Report_Id
+	message += ",ReportTime" + report.Report_Time
+	message += ",RequestTime:" + report.Request_Time
+	message += ",震度(Calcintensity):" + strconv.Itoa(report.Sindo())
+	message += ",緯度(latitude):" + strconv.FormatFloat(report.LatitudeF64(), 'f', 4, 64)
+	message += ",軽度(longitude):" + strconv.FormatFloat(report.LongitudeF64(), 'f', 4, 64)
+	message += ",地域(RegionName):" + report.Region_Name
+	message += ",マグニチュード:" + strconv.FormatFloat(report.MagunitudeF64(), 'f', 4, 64)
+
 	return message
 }
-
 
 func CreateMessageMaker(setting config.BotSetting) MessageMaker {
 	return MessageMaker{setting}
